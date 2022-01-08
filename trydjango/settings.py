3a94f5e42e80d40a342a80a6e2b4fe2e719c8ec3
XeyyamSherif <61112169+XeyyamSherif@users.noreply.github.com>
2022-01-08 17:59:10 +0400

    Update settings.py

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -89,12 +89,6 @@ WSGI_APPLICATION = 'trydjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 POSTGRES_DB = os.environ.get("POSTGRES_DB") #database name
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD") # database user password
